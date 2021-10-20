e2acc8580f306b28bc687aba25c5377518cc3b88
Stewart Glover <stewart.glover191@gmail.com>
2021-10-20 16:23:22 +0100

    updated seed file for user and messages. improved views for chatroom and sign up. created models for user and message

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  post 'message', to: 'messages#create'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def random_text
+  phrases = ['Hey mate', 'Sounds good', 'Have you seen the latest episode?', 'No thanks', 'Goodbye']
+  phrases.sample
+end
 
+puts 'Deleting Users...'
+Message.destroy_all
+User.destroy_all
 
 puts 'Creating Users.....'
 User.create(username: "Bob", password: '1234qwertyuiop')
@@ -14,3 +21,11 @@ User.create(username: "Jack", password: '1234qwertyuiop')
 User.create(username: "Tom", password: '1234qwertyuiop')
 User.create(username: "Cat", password: '1234qwertyuiop')
 puts 'Users created'
+
+puts 'Creating messages'
+users = User.all
+users.each do |user|
+  Message.create(body: random_text, user: user)
+end
+puts 'Messages created'
+
